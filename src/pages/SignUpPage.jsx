13cf666c555203968574a6c2c6a451aa755ee3c8
cherylruei <cherylrueichun@gmail.com>
2023-03-10 15:47:02 +0800

    feat: add register function to signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,25 +8,50 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { register } from 'api/auth';
+import Swal from 'sweetalert2';
 
 const SignUpPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassWord] = useState('');
   const [email, setEmail] = useState('');
 
-  // const handleClick = async () => {
-  //   if (username.length === 0) {
-  //     return;
-  //   }
-  //   if (password.length === 0) {
-  //     return;
-  //   }
-  //   if (email.length === 0) {
-  //     return;
-  //   }
+  const handleClick = async () => {
+    if (username.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+    if (email.length === 0) {
+      return;
+    }
 
-  //   const {success, authToken} = await 
-  // };
+    const { success, authToken } = await register({
+      username,
+      password,
+      email,
+    });
+
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+      Swal.fire({
+        position: 'top',
+        title: '註冊成功',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false,
+      });
+      return;
+    }
+    Swal.fire({
+      position: 'top',
+      title: '註冊失敗',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false,
+    });
+  };
 
   return (
     <AuthContainer>
@@ -63,7 +88,7 @@ const SignUpPage = () => {
           onChange={(passwordInputValue) => setPassWord(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
       <Link to="/login">
         <AuthLinkText>取消</AuthLinkText>
       </Link>
