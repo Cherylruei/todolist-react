282741bffc152aeef48e6d1a65e1ae7d911a43b7
cherylruei <cherylrueichun@gmail.com>
2023-03-10 11:56:45 +0800

    feat: login api function

---
+++ src/api/auth.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const authUrl = 'https://todo-list.alphacamp.io/api/auth';
+
+// 前端這邊會傳入 userName, password 傳入
+// 因為要等 data 拿到資料，要用非同步的方式 async await
+export const login = async ({ userName, password }) => {
+  // 因為在這裡使用 axios，所以 response 都會被封裝在 data的物件裡，要使用解構賦值把 data取出來
+  try {
+    const { data } = await axios.post(`${authUrl}/login`, {
+      userName,
+      password,
+    });
+
+    const { authToken } = data;
+    if (authToken) {
+      return { success: true, ...data };
+    }
+    return data;
+  } catch (error) {
+    console.error('[Login Failed]:', error);
+  }
+};
