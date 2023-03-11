c0c14e08312089672f8669365c2af530071a6a4f
cherylruei <cherylrueichun@gmail.com>
2023-03-11 12:39:57 +0800

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,19 @@
+import { useAuth } from 'contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const HomePage = () => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+  // 使用 useEffect 在 navigate 和 isAuthenticated 有變化時重新渲染
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
+
   return <div>HomePage</div>;
 };
 
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -8,7 +8,7 @@ const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, currentMember } = useAuth();
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -171,7 +171,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name} />
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
